ea4304410e77a5d5768cc986815ef65d696d72cd
guiguizada <guiguizada@gmail.com>
2019-11-21 00:53:28 -0300

    new file:   Ubuntu-R.ttf 	new file:   font.tres 	modified:   main.gd 	modified:   main.tscn

--- main.gd
+++ main.gd
@@ -15,7 +15,10 @@ var gelo
 var drink_fraco
 var drink_suave
 var drink_forte
+var paladar_do_drink
 var cuba_livre = false
+var price
+var failure_reason = "não é cuba livre por:\n"
 onready var option_button = get_node("refrigerante_slider/OptionButton")
 onready var rum_slider = get_node("rum_slider")
 onready var gelo_slider = get_node("gelo_slider")
@@ -51,18 +54,21 @@ func _ready():
 func _process(delta):
 	#interface de usuario
 	refrigerante_tipo = option_button.get_selected_id()
-	refrigerante_quantidade = refrigerante_slider.value
-	rum_quantidade = rum_slider.value
-	gelo_quantidade = gelo_slider.value
+
 	refrigerante_label.text = str(refrigerante_quantidade)+" ml"
 	gelo_label.text = str(gelo_quantidade)+" ml"
 	rum_label.text = str(rum_quantidade)+" ml"
+	#$rum_slider/LineEdit.text = str(rum_quantidade)+" ml"
+	refrigerante_quantidade = refrigerante_slider.value
+	rum_quantidade = rum_slider.value
+	gelo_quantidade = gelo_slider.value
 	#interface de usuario
 	
 	#calculo de grau de pertinencia
 	#rum
 	if rum_quantidade<10:
 		rum_fraco = 0
+		failure_reason += "-quantidade de rum menor que 10ml;\n"
 	if  rum_quantidade>=10 and rum_quantidade<15:
 		rum_fraco = 1
 	if rum_quantidade>=15 and rum_quantidade<=20:
@@ -87,20 +93,24 @@ func _process(delta):
 		rum_forte = 1
 	if rum_quantidade>30:
 		rum_forte = 0
+		failure_reason += "-quantidade de rum maior que 30ml\n"
 	#rum
 	#gelo
 	if gelo_quantidade<20:
 		gelo = 0
+		failure_reason += "-quantidade de gelo menor que 20ml\n"
 	if gelo_quantidade == 20:
 		gelo = 1
 	if gelo_quantidade>20:
 		gelo = 0
+		failure_reason += "-quantidade de gelo maior que 20ml\n"
 	#gelo
 	#refrigerante
 	#coca-cola
 	if option_button.get_selected_id() == 0:
 		if refrigerante_quantidade<50:
 			refrigerante_forte = 0
+			failure_reason += "-quantidade de coca-cola menor que 50ml\n"
 		if refrigerante_quantidade>=50 and refrigerante_quantidade<52:
 			refrigerante_forte = 1
 		if refrigerante_quantidade>=52 and refrigerante_quantidade<=54:
@@ -125,11 +135,13 @@ func _process(delta):
 			refrigerante_fraco = 1
 		if refrigerante_quantidade>60:
 			refrigerante_fraco = 0
+			failure_reason += "-quantidade de coca-cola maior que 60ml\n"
 	#coca-cola
 	#pepsi
 	if option_button.get_selected_id() == 1:
 		if refrigerante_quantidade<60:
 			refrigerante_forte = 0
+			failure_reason += "-quantidade de pepsi-cola menor que 60ml\n"
 		if refrigerante_quantidade>=60 and refrigerante_quantidade<62:
 			refrigerante_forte = 1
 		if refrigerante_quantidade>=62 and refrigerante_quantidade<=64:
@@ -154,25 +166,39 @@ func _process(delta):
 			refrigerante_fraco = 1
 		if refrigerante_quantidade>70:
 			refrigerante_fraco = 0
+			failure_reason += "-quantidade de pepsi-cola maior que 70ml\n"
 	#pepsi
 	#refrigerante
 	#calculo de grau de pertinencia
 	
-	#defuzzificacao
+	#defuzzificacao/output
 	drink_fraco = get_max(get_min(refrigerante_fraco, rum_fraco, gelo),get_min(refrigerante_fraco, rum_suave, gelo), get_min(refrigerante_suave, rum_fraco, gelo))
 	drink_suave = get_max(get_min(refrigerante_forte, rum_fraco, gelo), get_min(refrigerante_suave, rum_suave, gelo), get_min(refrigerante_fraco, rum_forte, gelo))
 	drink_forte = get_max(get_min(refrigerante_forte, rum_suave, gelo), get_min(refrigerante_forte, rum_forte, gelo), get_min(refrigerante_suave, rum_forte, gelo))
-	$Label2.text = str(drink_fraco)+", "+str(drink_suave)+", "+str(drink_forte)
 	if(drink_fraco>=drink_forte and drink_fraco>=drink_suave):
+		paladar_do_drink = drink_fraco
 		$Label.text = "drink fraco"
+		price = "R$ 15,00"
 		cuba_livre = true
 	if(drink_suave>=drink_forte and drink_suave>=drink_fraco):
+		paladar_do_drink = drink_suave
 		$Label.text = "drink suave"
+		price = "R$ 20,00"
 		cuba_livre = true
 	if(drink_forte>=drink_fraco and drink_forte>=drink_suave):
+		paladar_do_drink = drink_forte
 		$Label.text = "drink forte"
+		price = "R$ 25,00"
 		cuba_livre = true
 	if(drink_forte<=0 and drink_suave<=0 and drink_fraco<=0):
-		$Label.text = "nao e cuba livre"
+		paladar_do_drink = 0
+		$Label.text = "não é cuba livre"
+		price = "R$ 0,00"
 		cuba_livre = false
-	#defuzzificacao
+	$RichTextLabel2.text = "μDrinkFraco: "+str(drink_fraco)+"\nμDrinkSuave: "+str(drink_suave)+"\nμDrinkforte: "+str(drink_forte)+"\nμPaladarDoDrink: "+str(paladar_do_drink)
+	if cuba_livre:
+		failure_reason = ""
+	$RichTextLabel.text = failure_reason
+	failure_reason = "não é cuba livre por:\n"
+	$price.text = "preço: "+str(price)
+	#defuzzificacao/output
